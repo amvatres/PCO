dd95310643517dfc797986ab8b22db65bc94bdea
amela.vatre@stu.ibu.edu.ba <amela.vatres@stu.ibu.edu.ba>
2019-09-04 19:00:23 +0200

    charts pokusaj

--- app/controllers/dashboard-controller.js
+++ app/controllers/dashboard-controller.js
@@ -37,18 +37,16 @@ function dashboardController($scope,$http,$location,$routeParams,$route,toastr){
         });
       }
 
+      new Chartist.Pie('.ct-chart', {
+        series: [20, 10, 30, 40]
+      }, {
+        donut: true,
+        donutWidth: 60,
+        donutSolid: true,
+        startAngle: 270,
+        showLabel: true
+      });
+      
      
-      
-
-
-
-   
-      
-
-   
-  
-
-   
-
     
 }
